4d9c122a19d5f041689a79a9d6c5ae3cfd49b36a
Gerrit Boogaart <keesboog@gmail.com>
2023-03-11 11:51:16 -0500

    fix test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/ping/i);
+  const linkElement = screen.getByText(/Second Hello World/i);
   expect(linkElement).toBeInTheDocument();
 });
